5956a5324020a1b45fc5ab3259b506ad9014dbf5
Guilherme Lemos <guilhermelemos@MacBook-Pro-de-Guilherme.local>
2023-06-24 16:52:47 -0300

    first commit

---
+++ src/test/java/com/example/talkerManager/TalkerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talkerManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
